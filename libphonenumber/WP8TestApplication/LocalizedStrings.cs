98275ad62bccd20b34e0e11b5d9a8e2cee675ad4
Neeraj Gupta <neeraj.gupta983@gmail.com>
2014-12-05 01:49:06 +0530

    add wp8 test app

---
+++ libphonenumber/WP8TestApplication/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using WP8TestApplication.Resources;
+
+namespace WP8TestApplication
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
